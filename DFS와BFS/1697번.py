8d07d2e87ad295808fd34a9774ddeb29d783d100
songgplant <wogus0523@gmail.com>
2022-04-27 00:11:25 +0900

    220427-1

--- DFS와BFS/1697번.py
+++ DFS와BFS/1697번.py
@@ -1,2 +1,24 @@
 # 숨바꼭질
 # https://www.acmicpc.net/problem/1697
+from collections import deque
+
+N, K = map(int, input().split())
+# N은 출발, K는 동생위치=도착점
+max_num = 100000
+visited = [0]*(max_num+1)
+
+def bfs() :
+    queue = deque()
+    queue.append(N)
+    while queue :
+        x = queue.popleft()
+
+        if x == K :
+            print(visited[x])
+            break
+
+        for j in (x-1, x+1, x*2) :
+            if 0 <= j <= max_num and not visited[j] :
+                visited[j] = visited[x]+1
+                queue.append(j)
+bfs()
