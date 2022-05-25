b6cbc903d014e0905f2a88f2fb836ded40744372
songgplant <wogus0523@gmail.com>
2022-05-25 19:35:38 +0900

    220525 integer

---
+++ pythonbasic.py
@@ -0,0 +1,9 @@
+import sys
+
+# print(type(sys.maxsize))
+# print(sys.maxsize)
+print(type(9223372036854775807))
+print(type(9223372036854775807))
+
+print(0.1+0.2) # 0.30000000000000004 ???????
+print(float(0.1)==0.1)
